6c35f420291569fcaf54a857a6c8d0ef0e64cd07
Gustavo Caçador <gustavoirl456@outlook.com>
2026-03-20 01:00:52 -0300

    gravando os dados no banco

--- user-request/src/main/java/com/gustavoronchi/user_request/writer/InsertUserDataDBWriterConfig.java
+++ user-request/src/main/java/com/gustavoronchi/user_request/writer/InsertUserDataDBWriterConfig.java
@@ -1,15 +1,29 @@
 package com.gustavoronchi.user_request.writer;
 
 import com.gustavoronchi.user_request.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.infrastructure.item.ItemWriter;
+import org.springframework.batch.infrastructure.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.infrastructure.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class InsertUserDataDBWriterConfig {
 
+    private static Logger logger = LoggerFactory.getLogger(InsertUserDataDBWriterConfig.class);
+
     @Bean
-    public ItemWriter<User> insertUserDataDBWriter () {
-        return users -> users.forEach(System.out::println);
+    public ItemWriter<User> insertUserDataDBWriter (@Qualifier("appDB")DataSource dataSource) {
+        logger.info("[WRITER STEP] - Inserting user data");
+        return new JdbcBatchItemWriterBuilder<User>()
+                .dataSource(dataSource)
+                .sql("insert into tb_user (login, name, avatar_url) values (:login, :name, :avatarUrl)")
+                .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+                .build();
     }
 }
